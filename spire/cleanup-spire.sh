b882cd77f7b091707a9df134bb05a2afdc2715e2
Jimmy Song <rootsongjc@gmail.com>
2024-07-09 18:25:48 +0800

    chore: Update Spire configuration and installation scripts

--- spire/cleanup-spire.sh
+++ spire/cleanup-spire.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
-export CTX_CLUSTER1=foo-eks-cluster
-export CTX_CLUSTER2=bar-eks-cluster
+export CTX_CLUSTER1=cluster-1
+export CTX_CLUSTER2=cluster-2
 
 kubectl config use-context ${CTX_CLUSTER1}
 
--- spire/install-spire.sh
+++ spire/install-spire.sh
@@ -5,25 +5,24 @@ set -e
 export CTX_CLUSTER1=cluster-1
 export CTX_CLUSTER2=cluster-2
 
-spire_server_node_foo=$(kubectl get nodes -l dedicated=spire-server --context="${CTX_CLUSTER1}" -o jsonpath='{.items[*].metadata.name}')
-spire_server_node_bar=$(kubectl get nodes -l dedicated=spire-server --context="${CTX_CLUSTER2}" -o jsonpath='{.items[*].metadata.name}')
-
 # Install Spire on foo cluster
-kubectl apply -f ./spire/configmaps.yaml --context="${CTX_CLUSTER1}"
-cat ./spire/foo-spire.yaml | sed "s/<spire_server_node_bar>/$spire_server_node_bar/g" | kubectl apply -f - --context="${CTX_CLUSTER1}"
-
-kubectl -n spire rollout status statefulset spire-server --context="${CTX_CLUSTER1}"
-kubectl -n spire rollout status daemonset spire-agent --context="${CTX_CLUSTER1}"
-
-foo_bundle=$(kubectl exec --context="${CTX_CLUSTER1}" -c spire-server -n spire --stdin spire-server-0  -- /opt/spire/bin/spire-server bundle show -format spiffe -socketPath /run/spire/sockets/server.sock)
+kubectl apply -f ./spire/configmaps.yaml -f ./spire/foo-spire.yaml --context="${CTX_CLUSTER1}"
+spire_bundle_endpoint_foo=$(kubectl get svc spire-server-bundle-endpoint --context="${CTX_CLUSTER1}" -n spire -o jsonpath='{.status.loadBalancer.ingress[0].ip}:{.spec.ports[0].port}')
 
 # Install Spire on bar cluster
-kubectl apply -f ./spire/configmaps.yaml --context="${CTX_CLUSTER2}"
-cat ./spire/bar-spire.yaml | sed "s/<spire_server_node_foo>/$spire_server_node_foo/g" | kubectl apply -f - --context="${CTX_CLUSTER2}"
+kubectl apply -f ./spire/configmaps.yaml -f ./spire/bar-spire.yaml --context="${CTX_CLUSTER2}"
+spire_bundle_endpoint_bar=$(kubectl get svc spire-server-bundle-endpoint --context="${CTX_CLUSTER2}" -n spire -o jsonpath='{.status.loadBalancer.ingress[0].ip}:{.spec.ports[0].port}')
 
+# Restart Spire foo
+cat ./spire/foo-spire.yaml | sed "s/<spire_bundle_endpoint_bar>/$spire_bundle_endpoint_bar/g" | kubectl apply -f - --context="${CTX_CLUSTER1}"
+kubectl -n spire rollout status statefulset spire-server --context="${CTX_CLUSTER1}"
+kubectl -n spire rollout status daemonset spire-agent --context="${CTX_CLUSTER1}"
+foo_bundle=$(kubectl exec --context="${CTX_CLUSTER1}" -c spire-server -n spire --stdin spire-server-0  -- /opt/spire/bin/spire-server bundle show -format spiffe -socketPath /run/spire/sockets/server.sock)
+
+# Restart Spire bar
+cat ./spire/bar-spire.yaml | sed "s/<spire_bundle_endpoint_foo>/$spire_bundle_endpoint_foo/g" | kubectl apply -f - --context="${CTX_CLUSTER2}"
 kubectl -n spire rollout status statefulset spire-server --context="${CTX_CLUSTER2}"
 kubectl -n spire rollout status daemonset spire-agent --context="${CTX_CLUSTER2}"
-
 bar_bundle=$(kubectl exec --context="${CTX_CLUSTER2}" -c spire-server -n spire --stdin spire-server-0 -- /opt/spire/bin/spire-server bundle show -format spiffe -socketPath /run/spire/sockets/server.sock)
 
 # Set foo.com bundle to bar.com SPIRE bundle endpoint
